031939a75cc2f8501b73b8ce0dddebbf459879e8
allison d manzo <adm52@afsconnect1.njit.edu>
2017-03-21 20:26:03 -0400

    added and edited for hw

--- ch08_ex2/index.php
+++ ch08_ex2/index.php
@@ -19,23 +19,25 @@ switch ($action) {
         $scores = $_POST['scores'];
 
         // validate the scores
-        // TODO: Convert this if statement to a for loop
-        if (empty($scores[0]) ||
-            empty($scores[1]) ||
-            empty($scores[2]) ||
-            !is_numeric($scores[0]) ||
-            !is_numeric($scores[1]) ||
-            !is_numeric($scores[2])) {
-                $scores_string = 'You must enter three valid numbers for scores.';
-                break;
-        }
+        $valid = true;
+	for ($i = 0; $i <count($scores); $i++) {
+	    if (empty($scores[$i]) || !is_numeric($scores[$i])) {
+	      $scores_string = 'You must enter three valid numbers for scores.';
+	      $valid=false;
+	      break;
+	    }
+	}
+
+	if (!$valid) {
+	    break;
+	}
 
         // process the scores
-        // TODO: Add code that calculates the score total
         $scores_string = '';
         foreach ($scores as $s) {
             $scores_string .= $s . '|';
-        }
+            $score_total += $s;
+	}
         $scores_string = substr($scores_string, 0, strlen($scores_string)-1);
 
         // calculate the average
@@ -54,14 +56,12 @@ switch ($action) {
         $count = 0;
         $max_rolls = -INF;
 
-        // TODO: convert this while loop to a for loop
-        while ($count < 10000) {
+	for ($count = 0; $count <10000; $count++) {
             $rolls = 1;
             while (mt_rand(1, 6) != 6) {
                 $rolls++;
             }
             $total += $rolls;
-            $count++;
             $max_rolls = max($rolls, $max_rolls);
         }
         $average_rolls = $total / $count;
